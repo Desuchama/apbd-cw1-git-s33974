16a718613e948fcdc3e7b425c8e01e38c803ee3d
Bruno Bednarski <s33974@pjwstk.edu.pl>
2026-03-19 06:28:16 +0100

    Second attempt at conflict creation in feature-conflict

--- src/StatisticsHelper.java
+++ src/StatisticsHelper.java
@@ -33,6 +33,7 @@ public class StatisticsHelper {
         for (int i = 1; i < values.length; i++) {
             if (result>values[i])
                 result = values[i];
+            result += 1;
         }
         return result;
     }
