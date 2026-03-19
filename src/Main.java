4609657a022d5874d3a849e2ea09171f0938b923
Bruno Bednarski <s33974@pjwstk.edu.pl>
2026-03-19 02:20:28 +0100

    initialized array[4] of double[4] arrays

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         double[] X = {10, 12}, Y = {14, 15};
-        ;
 
         System.out.println(dist(new double[] {3, 5}, Y));
 
+        double[] X1 = new double[4], X2 = new double[4], X3 = new double[4], X4 = new double[4];
+        double[][] array = new double[][] {X1, X2, X3, X4};
+        System.out.println(array.toString());
     }
     public static double dist(double[] x, double[] y) {
         double[] result = new double[x.length];
