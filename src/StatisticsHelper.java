a7774212772501c44eca6060f5c990f02f87c4d9
Bruno Bednarski <s33974@pjwstk.edu.pl>
2026-03-19 04:39:57 +0100

    Method calculateAverage(int[] values) added to StatisticsHelper

--- src/StatisticsHelper.java
+++ src/StatisticsHelper.java
@@ -6,6 +6,15 @@ public class StatisticsHelper {
 
     public StatisticsHelper(int[] input){
         this.data = input;
+        this.average = calculateAverage(input);
+    }
+    public double calculateAverage(int[] values){
+        double result = 0;
+        for (int i = 0; i < values.length; i++) {
+            result += values[i];
+            }
+        result /= values.length;
+        return result;
     }
     public String toString(){
         return average + " is the average of: " + Arrays.toString(data);
